6e3550ba309a40d6b005095ef1636a34c365ed7b
impliedfeline <kinnunen.jussi@hotmail.com>
2019-07-04 19:02:58 +0300

    uibuild

---
+++ build/precache-manifest.ef0980a34941158ed922a942c9538cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e4b7a43a812580d6ee2cf664310624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc3af4517ede06691342",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f96b81fb72c0a32392df",
+    "url": "/static/js/2.62584cbd.chunk.js"
+  },
+  {
+    "revision": "cc3af4517ede06691342",
+    "url": "/static/js/main.e8cd982e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
