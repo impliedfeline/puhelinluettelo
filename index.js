9ff012e4a79f5a8e806feaf925f56d9fb2814d35
impliedfeline <kinnunen.jussi@hotmail.com>
2019-07-05 22:54:47 +0300

    Korjaa form validointi

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.put('/api/persons/:id', (req, res, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(req.params.id, person, { runValidators: true, new: true })
     .then(updatedPerson => {
       res.json(updatedPerson.toJSON())
     })
